19ad3059c4b914acdad9e3c6168b0668390e2b24
roofimon <roofimon@gmail.com>
2016-02-23 16:35:57 +0700

    Refactor

--- src/scripts/fizzbuzz.ts
+++ src/scripts/fizzbuzz.ts
@@ -2,8 +2,6 @@ class Fizzbuzz {
   count(value: number): string {
     if(value == 3)
       return "fizz";
-    if(value == 1)
-      return "1";
-    return "2";
+    return value.toString();
   }
 }
